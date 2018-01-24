127017495a77988ef7d44d331760835542f8249d
Liakh Vladyslav <wlodzislaw.lw@gmail.com>
2018-01-24 20:57:49 +0200

    v0.5
    Basic mechanic done

--- js/mnu.js
+++ js/mnu.js
@@ -3,3 +3,8 @@ $(document).ready(function() {
     $('.off-canvas').toggleClass('open');
   });
 })
+$(document).ready(function() {
+  $('.mnu-toggle').on('click', function(){
+    $('.nav-bg').toggleClass('open');
+  });
+})
